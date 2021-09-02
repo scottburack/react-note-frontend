b34d15d9647d9d0b3a28829412bbbeff9241101c
Scott Burack <itsmescottb123@gmail.com>
2021-09-02 17:34:20 -0400

    new react note project

---
+++ src/app.jsx
@@ -0,0 +1,97 @@
+class Todo extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = { done: props.done,
+                       text: props.text };
+
+        this.handleClick = this.handleClick.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleClick(event) {
+      this.setState(state => ({
+          done: !state.done
+      }),
+      function(event) {
+          this.handleSubmit(event)
+      }
+      )
+    }
+
+    handleChange(event) {
+        let text = event.target.value
+        this.setState(state => ({
+            text: text
+        }))
+    }
+
+    handleSubmit(event) {
+        console.log("this is where the submit will happen")
+    //   this.setState(state => ({
+          
+    //   }))
+    }
+
+    render() {
+
+        return <div className="todo">
+                 <span>
+                   <input type="checkbox" checked={this.state.done} onClick={this.handleClick} /> 
+                   <input type="text" className={(this.state.done) ? 'done' : 'not-done' } value={this.state.text} onChange={this.handleChange} onBlur={this.handleSubmit} />
+                 </span>
+               </div>
+    }
+}
+
+class TodoList extends React.Component {
+  constructor(props) {
+      super(props)
+
+      this.state = { todos: [
+          {
+              _id: 1,
+              text: 'Item 1',
+              done: false
+          },
+          {
+            _id: 2,
+            text: 'Item 2',
+            done: true
+        }
+      ]}
+      
+      this.newTodo = this.newTodo.bind(this)
+  }
+
+  newTodo(event) {
+      event.preventDefault()
+
+      todos = this.state.todos
+      todos.push({ _id: ""})
+
+      this.setState(state => ({
+          todos: todos
+      }))
+  }
+
+  render() {
+      const todoList = this.state.todos.map((todo) => 
+        <Todo key={todo._id.toString()} text={todo.text} done={todo.done} />
+      )
+
+      return <React.Fragment>
+               <h1>React Todo App!</h1> 
+               {todoList}
+                <a href="#" onClick={this.newTodo}>New Todo</a>
+             </React.Fragment>
+  }
+}
+
+
+ReactDOM.render(
+    <TodoList/>,
+    document.getElementById('root')
+)
